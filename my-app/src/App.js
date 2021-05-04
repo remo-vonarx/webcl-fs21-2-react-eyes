1a465075851b2fc0a5b379e0b4b1986ebf364316
FXFrosties <kym.cyril@gmail.com>
2021-05-04 13:35:30 +0200

    initial setup

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Eye from './Eye'
+
+function App() {
+  return (
+    <div className="App">
+      <Eye></Eye>
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/Eye.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+class Eye extends React.Component{
+  constructor(){
+    super()
+    this.state= {
+      isOpen: false
+    }
+  }
+
+  
+
+  render(){
+    return(
+      <div>
+        <p>Hello</p>
+        <p>Hello</p>
+      </div>
+    )
+  }
+
+}
+
+export default Eye;
